5e03a191c3442c20a7ab9830a1d145fe154ab130
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2021-06-23 17:48:19 +0930

    Contact works

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,44 @@
-import React from "react";
-
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import apiKeys from "../apikeys";
 function Contact() {
+  const [formInfo, setFormInfo] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormInfo({
+      ...formInfo,
+      [name]: value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .send(apiKeys.SERVICE_ID, apiKeys.TEMPLATE_ID, formInfo, apiKeys.USER_ID)
+      .then(
+        function (response) {
+          alert(
+            "You're email has gone through, I will get back to you shortly!"
+          );
+          setFormInfo({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        function (error) {
+          alert(
+            "Sorry, there was some sort of error, please try again or email me at: benjamincyn@gmail.com"
+          );
+        }
+      );
+  };
+
   return (
     <section className="container">
       <div className="row">
@@ -20,14 +58,20 @@ function Contact() {
                   className="form-control information"
                   type="text"
                   placeholder="Name"
+                  name="name"
+                  value={formInfo.name}
+                  onChange={handleFormChange}
                 />
               </div>
               <div className="form-group">
-                <label className="contactHeader">Email address</label>
+                <label className="contactHeader">Your Email address</label>
                 <input
                   type="email"
                   className="form-control information"
                   placeholder="name@example.com"
+                  name="email"
+                  value={formInfo.email}
+                  onChange={handleFormChange}
                 />
               </div>
               <div className="form-group">
@@ -35,12 +79,16 @@ function Contact() {
                 <textarea
                   className="form-control information"
                   rows="3"
+                  name="message"
+                  value={formInfo.message}
+                  onChange={handleFormChange}
                 ></textarea>
               </div>
               <button
                 type="submit"
                 id="submitContact"
                 className="btn btn-primary"
+                onClick={onSubmit}
               >
                 Submit
               </button>
