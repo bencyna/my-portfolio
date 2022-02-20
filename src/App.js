59eb1e005aeefdced3a8f85559bd7af799a9b5e6
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2022-02-20 17:50:47 +1030

    add navabr and mobile responsivneess

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/navbar"
+import Contact from "./Pages/Contact/index"
+import About from "./Pages/About//index"
+import Portfolio from "./Pages/Portfolio/index"
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar/>
         <Routes>
-          <Route path="/" element={<Navbar/>} />  
+          <Route path="/contact" element={<Contact/>} />  
+          <Route path="/about" element={<About/>} />  
+          <Route path="/" element={<Portfolio/>} />  
         </Routes>  
       </div>
     </Router>
---
+++ src/Pages/Portfolio/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Projects from "../../Components/Projects"
+
+
+export default function Portfolio() {
+  return (
+    <div>Portfolio
+        <Projects/>
+    </div>
+
+  )
+}
