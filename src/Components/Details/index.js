eb0e57ba2d251e65cd988948e119d402aee04062
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2022-02-27 21:18:58 +1030

    added details to mu projects page

---
+++ src/Components/Details/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import resume from "../../assets/Resume.pdf";
+
+function Details() {
+  return (
+    <div className="row information">
+      <div className="col-lg-3">
+        <address>
+          {" "}
+          <i className="fas fa-signature"></i> Benjamin Cyna
+        </address>
+      </div>
+      <div className="col-lg-4">
+        <address>
+          <i className="fas fa-envelope-square"></i> benjamincyna@gmail.com
+        </address>
+      </div>
+      <div className="col-lg-3">
+        <address>
+          <i className="fas fa-phone"></i> 0430653816
+        </address>
+      </div>
+      <div className="col-lg-2">
+        <span title="Veiw Resume" className="columns colourDisplay is-mobile">
+          <button id="resume">
+            <Link
+              to={{
+                pathname: resume
+                  // "https://docs.google.com/document/d/1D8BpELWO2wDjZwn11Bb6IZDXPsnFj4eDMavnGDE7F3E/",
+              }}
+              target="_blank"
+            >
+              <i className="far fa-file"></i>
+            </Link>
+          </button>
+        </span>
+      </div>
+      <div className="col-lg-5 linkedIn">
+        <a href="https://www.linkedin.com/in/bencyna567/">
+          <i className="fab fa-linkedin"></i> LinkedIn
+        </a>
+      </div>
+      <div className="col-lg-7 github">
+        <a href="https://github.com/bencyna/">
+          <i className="fab fa-github"></i> Github
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Details;
--- src/Pages/Portfolio/index.js
+++ src/Pages/Portfolio/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Projects from "../../Components/Projects"
+import Details from "../../Components/Details"
 
 
 export default function Portfolio() {
   return (
     <div>Portfolio
+        <Details/>
         <Projects/>
     </div>
 
