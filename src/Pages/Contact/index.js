1404deb8a7a5b600cecb9f163719b93ff24318ff
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2022-03-30 13:03:59 +1030

    add mobile responsiveness to contact form

--- src/Pages/Contact/index.js
+++ src/Pages/Contact/index.js
@@ -9,7 +9,7 @@ export default function Contact({contact_display, setContactDisplay}) {
 
 
   return (
-    <div className={`z-50 bg-white w-1/3 h-4/6 fixed bottom-0 right-0 border-x ${contact_display}`}>
+  <div className={`z-50 bg-white md:w-2/3 lg:w-1/3 w-full md:h-4/6 lg:h-4/6 h-5/6 fixed bottom-0 right-0 border-x ${contact_display}`}>
       <div className="bg-gray-100 w-full h-1/12 border border-gray-200">
         <div className="w-1/2 float-right bg-gray-100 "> 
          <Minimize2 className="p-1 float-right mr-8 cursor-pointer" onClick={hideDisplay}/>
